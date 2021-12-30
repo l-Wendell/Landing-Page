91bbc1a94142fc6f392ebbc59d2e668148c1ccb9
lucas <lucaswhendellf@gmail.com>
2021-12-30 17:31:38 -0300

    Landing Commit

---
+++ script.js
@@ -0,0 +1,46 @@
+function typingEffect() {
+    const contactTexts = shuffleArray(['O que ela faz por você?']);
+    const typedtext = document.getElementsByClassName("conteudo-secundario-titulo")[0];
+    let removing = false;
+    let idx = char = 0;
+
+    setInterval(() => {
+
+        if (char < contactTexts[idx].length) typedtext.innerHTML += contactTexts[idx][char];
+
+        if (char == contactTexts[idx].length + 15) removing = true;
+
+        if (removing) typedtext.innerHTML = typedtext.innerHTML.substring(0, typedtext.innerHTML.length - 1);
+
+        char++;
+
+        if (typedtext.innerHTML.length === 0) {
+
+            if (idx === contactTexts.length - 1) idx = 0
+            else idx++;
+
+            char = 0;
+            removing = false;
+        }
+
+    }, 150);
+
+}
+
+typingEffect();
+function shuffleArray(array) {
+    let currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
